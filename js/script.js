61823a9beb12f852deb8978c30689d2a6646565b
cardoso-topdev <oscar@cardoso-devstar.com>
2022-01-31 12:20:58 -0600

    updated testimonials

--- js/script.js
+++ js/script.js
@@ -30,5 +30,27 @@
       $('.modal').fadeOut();
       $('.modal-overlay').fadeOut();
     })
+    $('.video-buttons .btn-theme').on('click', function () {
+      if ( !$(this).hasClass('active') ) {
+        $('.video-player .btn-theme.active').removeClass('active');
+        var themeAnchor = $(this).attr('theme-anchor');
+        var killAnchor = $('.video-buttons .btn-wrap.active .btn-kill').attr('kill-anchor');
+        $('.video-player video.active').fadeOut(300, function() {
+          $('.video-player video[theme-anchor="' + themeAnchor + '"][kill-anchor="' + killAnchor + '"]').fadeIn().addClass('active');
+        });
+        $(this).addClass('active');
+      }
+    });
+    $('.video-buttons .btn-kill').on('click', function () {
+      if ( !$(this).parent().hasClass('active') ) {
+        $('.video-player .btn-wrap.active').removeClass('active');
+        var killAnchor = $(this).attr('kill-anchor');
+        var themeAnchor = $('.video-buttons .btn-theme.active').attr('theme-anchor');
+        $('.video-player video.active').fadeOut(300, function() {
+          $('.video-player video[theme-anchor="' + themeAnchor + '"][kill-anchor="' + killAnchor + '"]').fadeIn().addClass('active');
+        });
+        $(this).parent().addClass('active');
+      }
+    });
   });
 })(jQuery);
