46207b326fd18886b988db8d494268ae900a8721
cardoso-topdev <oscar@cardoso-devstar.com>
2022-01-31 03:28:09 -0600

    complete UI

--- js/script.js
+++ js/script.js
@@ -13,5 +13,22 @@
       centerMode: true,
       variableWidth: true,
     });
+    $(window).scroll(function(){
+      var header_trigger = $('.banner').outerHeight();
+      if ($(this).scrollTop() > header_trigger) {
+        $('.header').addClass('scrolled');
+      } else {
+        $('.header').removeClass('scrolled');
+      }
+    });
+    $('.modal-trigger').on('click', function(e) {
+      e.preventDefault();
+      $('.modal').fadeIn();
+      $('.modal-overlay').fadeIn();
+    })
+    $('.modal-overlay').on('click', function() {
+      $('.modal').fadeOut();
+      $('.modal-overlay').fadeOut();
+    })
   });
 })(jQuery);
